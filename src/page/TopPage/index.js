250cb17ac55055f0cb59a901fc7ca951feefb1c8
NozomiSugiyama <rioc.sugiyama@gmail.com>
2018-05-02 20:44:31 +0900

    update Top Page

--- src/page/TopPage/index.js
+++ src/page/TopPage/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Text, View } from "react-native"
 import { Actions } from "react-native-router-flux"
-import { Button, Heading, Avatar, FlexBox, Switch } from "react_native_template_1/src/components"
+import { Page, Button, Heading, Avatar, FlexBox, Switch } from "react_native_template_1/src/components"
 import { ListGroup, ListGroupItem } from "react_native_template_1/src/components/listGroup"
 import avatarImage from "react_native_template_1/assets/images/avatar.jpg"
 
@@ -17,47 +17,49 @@ export default class extends React.Component {
 
   render() {
     return (
-      <FlexBox 
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="space-around"
-        style={styles.host}
-      >
-        <View>
-          <Avatar
-            size="xlarge"
-            source={avatarImage}
+      <Page> 
+        <FlexBox
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-around"
+          style={styles.host}
+        >
+          <View>
+            <Avatar
+              size="xlarge"
+              source={avatarImage}
+            />
+            <Heading size="large">template 1</Heading>
+          </View>
+          <View>
+            <Heading size="small" style={{alignSelf: "flex-start"}}>components</Heading>
+            <ListGroup
+              style={{
+                width: 300
+              }}
+            >
+              <ListGroupItem onPress={() => Actions.alertComponentPage()}>Alert</ListGroupItem>
+              <ListGroupItem onPress={() => Actions.avatarComponentPage()}>Avatar</ListGroupItem>
+              <ListGroupItem onPress={() => Actions.badgeComponentPage()}>Badge</ListGroupItem>
+              <ListGroupItem onPress={() => Actions.buttonComponentPage()}>Button</ListGroupItem>
+              <ListGroupItem onPress={() => Actions.buttonGroupComponentPage()}>ButtonGroup</ListGroupItem>
+              <ListGroupItem onPress={() => Actions.CheckboxComponentPage()}>Checkbox</ListGroupItem>
+              <ListGroupItem onPress={() => Actions.headingComponentPage()}>Heading</ListGroupItem>
+              <ListGroupItem onPress={() => Actions.listGroupComponentPage()}>ListGroup</ListGroupItem>
+            </ListGroup>
+          </View>
+          <Switch
+            onPress={() => this.setState({switch1Enabled: !this.state.switch1Enabled})}
+            enabled={this.state.switch1Enabled}
           />
-          <Heading size="large">template 1</Heading>
-        </View>
-        <View>
-          <Heading size="small" style={{alignSelf: "flex-start"}}>components</Heading>
-          <ListGroup
-            style={{
-              width: 300
-            }}
-          >
-            <ListGroupItem onPress={() => Actions.alertComponentPage()}>Alert</ListGroupItem>
-            <ListGroupItem onPress={() => Actions.avatarComponentPage()}>Avatar</ListGroupItem>
-            <ListGroupItem onPress={() => Actions.badgeComponentPage()}>Badge</ListGroupItem>
-            <ListGroupItem onPress={() => Actions.buttonComponentPage()}>Button</ListGroupItem>
-            <ListGroupItem onPress={() => Actions.buttonGroupComponentPage()}>ButtonGroup</ListGroupItem>
-            <ListGroupItem onPress={() => Actions.CheckboxComponentPage()}>Checkbox</ListGroupItem>
-            <ListGroupItem onPress={() => Actions.headingComponentPage()}>Heading</ListGroupItem>
-            <ListGroupItem onPress={() => Actions.listGroupComponentPage()}>ListGroup</ListGroupItem>
-          </ListGroup>
-        </View>
-        <Switch
-          onPress={() => this.setState({switch1Enabled: !this.state.switch1Enabled})}
-          enabled={this.state.switch1Enabled}
-        />
-        <Switch 
-          disabledText="no"
-          enabledText="yes"
-          onPress={() => this.setState({switch2Enabled: !this.state.switch2Enabled})}
-          enabled={this.state.switch2Enabled}
-        />
-      </FlexBox>
+          <Switch 
+            disabledText="no"
+            enabledText="yes"
+            onPress={() => this.setState({switch2Enabled: !this.state.switch2Enabled})}
+            enabled={this.state.switch2Enabled}
+          />
+        </FlexBox>
+      </Page>
     );
   }
 }
