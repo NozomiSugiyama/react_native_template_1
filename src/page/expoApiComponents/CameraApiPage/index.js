d83b2023db059dca84a100442d3255210d0c580d
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-05-22 15:29:16 +0900

    modified camera

--- src/page/expoApiComponents/CameraApiPage/index.js
+++ src/page/expoApiComponents/CameraApiPage/index.js
@@ -2,65 +2,59 @@ import React from "react"
 import { Text, View } from "react-native"
 import { Actions } from "react-native-router-flux"
 import { Image, Button, Alert, Heading, Page } from "react_native_template_1/src/components"
-import { Camera, Permissions } from 'expo'
+import { ImagePicker } from 'expo'
 
 import styles from "./styles"
 
 // https://docs.expo.io/versions/v27.0.0/sdk/camera
 
 export default class extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-  };
 
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+  componentWillMount() {
+    this.setState({
+      image: null
+    })
   }
 
-  render() {
-    const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <Page
-          style={styles.host}
-          {...this.props}
-        >
-          <Heading>Camera</Heading>
-          <View
-            style={
-              styles.content
-            }>
-            <Camera
-              style={
-                styles.camera
-              }
-              type={this.state.type}>
-              <View
-                style={
-                  styles.stab
-                }>
-              </View>
-            </Camera>
-            <Button
-              onPress={() => {
-                this.setState({
-                  type: this.state.type === Camera.Constants.Type.back
-                    ? Camera.Constants.Type.front
-                    : Camera.Constants.Type.back,
-                });
-              }}
-            >
-              change
-            </Button>
-          </View>
-        </Page>
-      );
+  _takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: false
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({image: result.uri});
     }
+  };
+
+  render() {
+    let { image } = this.state;
+
+    return (
+      <Page
+        style={styles.host}
+        {...this.props}
+      >
+        <Heading>Camera</Heading>
+        <View
+          style={
+            styles.content
+          }>
+          <Button
+            onPress={this._takePhoto}
+          >
+            open
+          </Button>
+          {
+            image &&
+            <Image
+              source={{uri: image}}
+              style={{width: 300, height: 300}}
+            />
+          }
+        </View>
+      </Page>
+    );
   }
 }
